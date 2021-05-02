e65af01e427837855efde0d1d0c32cd62decce05
Krish Shah <63075612+krish-shahh@users.noreply.github.com>
2021-05-01 23:26:33 -0400

    Create target_calculator_gpa.js

---
+++ js/target_calculator_gpa.js
@@ -0,0 +1,29 @@
+function GoalCalc() {
+function round2d(n){return(.01* Math.round(100*n));}
+	// set variables
+	var currGPAnum = 0;
+	var currCredTotal = 0;
+	var goalGPAnum = 0;
+	var newCreditnum = 0;
+
+	// retrieve user input
+	currGPAnum = parseFloat(document.GoalGPACalc.currGPA.value);
+	cumCredTotal = parseFloat(document.GoalGPACalc.currCredits.value);
+	goalGPAnum = parseFloat(document.GoalGPACalc.goalGPA.value);
+	newCreditnum = parseFloat(document.GoalGPACalc.newCredits.value);
+
+	// find goal points
+	var points1 = 0;
+	points1 = goalGPAnum * (cumCredTotal + newCreditnum);
+
+	// Find points needed now
+	points1 = points1 - (currGPAnum * cumCredTotal);
+
+	// Calculate needed GPA
+	var GPA1 = 0;
+	GPA1 = round2d(points1 / newCreditnum);
+
+	alert("To reach your goal, your GPA for your next " + eval(newCreditnum) + " credits must be " + eval(GPA1));
+
+	return 0;
+}
