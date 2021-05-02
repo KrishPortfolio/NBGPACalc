7db271eb80d7f7df74fb5e79b17d9edf76a39259
Krish Shah <63075612+krish-shahh@users.noreply.github.com>
2021-05-02 10:20:29 -0400

    Update target_calculator_gpa.js

--- js/target_calculator_gpa.js
+++ js/target_calculator_gpa.js
@@ -23,6 +23,8 @@ function round2d(n){return(.01* Math.round(100*n));}
 	var GPA1 = 0;
 	GPA1 = round2d(points1 / newCreditnum);
 	
+	document.querySelector('.value').innerHTML = ("To reach your goal, your GPA for your next " + eval(newCreditnum) + " credits must be " + eval(GPA1));
+	
 	document.getElementById("target").innerHTML= ("To reach your goal, your GPA for your next " + eval(newCreditnum) + " credits must be " + eval(GPA1));
 
 	window.alert("To reach your goal, your GPA for your next " + eval(newCreditnum) + " credits must be " + eval(GPA1));
